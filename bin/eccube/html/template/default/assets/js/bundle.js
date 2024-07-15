e54deaf2861c6dce952dc40eeee25da12409703c
Koji Murakami <kojimura@gmail.com>
2024-07-15 21:18:47 +0900

    move-eccube

--- bin/eccube/html/template/default/assets/js/bundle.js
+++
@@ -1,8 +0,0 @@
-const $ = require('jquery');
-global.$ = global.jQuery = $;
-
-require('slick-carousel');
-require('slick-carousel/slick/slick.css');
-require('slick-carousel/slick/slick-theme.css');
-
-require('bootstrap');
--- bin/eccube/html/template/install/assets/js/bundle.js
+++
@@ -1,6 +0,0 @@
-const $ = require('jquery');
-global.$ = global.jQuery = $;
-
-require('bootstrap/dist/css/bootstrap.min.css');
-const bootstrap = require('bootstrap');
-global.bootstrap = bootstrap;
